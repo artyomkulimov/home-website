2ab6e3b3fe96d385a9b861ecd518d0dad93ea5b9
Wumpie <artyomkulimov@gmail.com>
2021-08-25 13:07:57 +0400

    what have you done

--- forms.py
+++ forms.py
@@ -5,8 +5,13 @@ from wtforms.fields.simple import PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators = [DataRequired(),Length(min = 2,max=20)])
-    email = StringField("Email",validators=[DataRequired(),Email()])
-    password = PasswordField("Password",validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password",validators=[DataRequired(), EqualTo(password)])
+    username = StringField("Username:", validators = [DataRequired(),Length(min = 2,max=20)])
+    email = StringField("Email:",validators=[DataRequired(),Email()])
+    password = PasswordField("Password:",validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password:",validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username:", validators = [DataRequired(),Length(min = 2,max=20)])
+    password = PasswordField("Password:",validators=[DataRequired()])
     submit = SubmitField("Register")
--- web_blog.py
+++ web_blog.py
@@ -1,30 +1,61 @@
-from flask import Flask,render_template,url_for
-from forms import RegistrationForm
+from flask import Flask,render_template,url_for,redirect,flash
+from forms import RegistrationForm,LoginForm
 import os
+from werkzeug.datastructures import Headers
+from flask_sqlalchemy import SQLAlchemy, model
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ['flask_secret_key']
+app.config["SECRET_KEY"] = os.environ['FLASK_SECRET_KEY']
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(20),nullable = False)
+    email = db.Column(db.String(30),nullable = False)
+    password = db.Column(db.String(20),nullable = False)
 
 @app.route("/home")
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",title = "Home",Header = "Website Blog")
 
 @app.route("/credits")
 def credits():
-    return render_template("credits.html")
+    return render_template("credits.html",title = "Credits",Header = "This is the credits page")
 
 @app.route("/form")
 def form():
-    return render_template("form.html")
+    return render_template("form.html",title = "Form")
 
 @app.route("/updates")
 def updates():
-    return render_template("updates.html")
-##############################################
-@app.route("/register")
+    return render_template("updates.html",title = "Updates",Header = "This is where I document updates to my github page.")
+
+@app.route("/register",methods = ["GET","POST"])
 def register():
-    return render_template("register.html")
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account was created for user {form.username.data}","success") 
+        return redirect(url_for("home"))
+    return render_template("register.html",form = form,title = "Register",Header = "This is the registration Form")
+
+@app.route("/login",methods = ["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if True: 
+            flash(f"You have been logged in to {form.username.data}","success") 
+            return redirect(url_for("home"))
+        else:
+            flash(f"Either username or password incorrect","inv-cred")
+    return render_template("login.html",form = form,title = "Login",Header = "This is the Login page")
+#################################
+
+
+
+
 
 
 
