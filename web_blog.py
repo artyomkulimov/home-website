47c134281d0ecba8d9e33c9c75f19376548cf058
Wumpie <artyomkulimov@gmail.com>
2021-08-26 16:21:03 +0400

    what have you done

--- web_blog.py
+++ web_blog.py
@@ -1,4 +1,8 @@
-from flask import Flask,render_template,url_for,redirect,flash
+from flask import Flask,render_template,url_for,redirect,flash, jsonify
+from flask_bcrypt import Bcrypt
+from flask_login import login_manager, login_user, current_user, logout_user, LoginManager, UserMixin
+from flask_login.mixins import UserMixin
+from wtforms.validators import Email
 from forms import RegistrationForm,LoginForm
 import os
 from werkzeug.datastructures import Headers
@@ -8,12 +12,17 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ['FLASK_SECRET_KEY']
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(20),nullable = False)
     email = db.Column(db.String(30),nullable = False)
     password = db.Column(db.String(20),nullable = False)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route("/home")
 @app.route("/")
@@ -34,9 +43,12 @@ def updates():
 
 @app.route("/register",methods = ["GET","POST"])
 def register():
-
     form = RegistrationForm()
     if form.validate_on_submit():
+        hash_password = bcrypt.generate_password_hash(form.password.data).decode("UTF-8")
+        user = User(username = form.username.data,email = form.email.data, password = hash_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f"Account was created for user {form.username.data}","success") 
         return redirect(url_for("home"))
     return render_template("register.html",form = form,title = "Register",Header = "This is the registration Form")
@@ -45,7 +57,9 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if True: 
+        user = User.query.filter_by(username = form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data): 
+            login_user(user)
             flash(f"You have been logged in to {form.username.data}","success") 
             return redirect(url_for("home"))
         else:
